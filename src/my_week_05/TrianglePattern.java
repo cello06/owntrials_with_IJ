158e8ef03505884e202331a2c72998a9ab2e2164
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-09 19:19:54 +0300

    live_class project

--- src/my_week_05/TrianglePattern.java
+++ src/my_week_05/TrianglePattern.java
@@ -23,23 +23,23 @@ public class TrianglePattern {
         System.out.println("\nPattern C\n");
 
         for (int i = 1; i <= 6; i++) {
-            for (int j = 1; j <=6-i ; j++) {
+            for (int j = 1; j <= 6 - i; j++) {
                 System.out.print("  ");
             }
-            for (int k = i; k >=1 ; k--) {
-                System.out.print(k+" ");
+            for (int k = i; k >= 1; k--) {
+                System.out.print(k + " ");
             }
             System.out.println();
         }
         System.out.println("---------------------------");
         System.out.println("\nPattern D\n");
 
-        for (int i = 1; i <=6 ; i++) {
-            for (int j = 1; j <i ; j++) {
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j < i; j++) {
                 System.out.print("  ");
             }
-            for (int k = 1; k <=7-i ; k++) {
-                System.out.print(k+" ");
+            for (int k = 1; k <= 7 - i; k++) {
+                System.out.print(k + " ");
             }
             System.out.println();
         }
