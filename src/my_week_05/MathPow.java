c084b0fefc8884e036c19e720e0e1005279b7ec4
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-09-09 17:18:13 +0300

    Password project

---
+++ src/my_week_05/MathPow.java
@@ -0,0 +1,30 @@
+package my_week_05;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class MathPow {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number : ");
+
+        int number = input.nextInt();
+
+        System.out.print("Pow Number : ");
+
+        int powNumber = input.nextInt();
+
+        int sum = 1;
+
+
+        for (int i = 1; i <= powNumber; i++) {
+
+            sum *= number;
+
+        }
+
+        System.out.print("Result : " + sum);
+    }
+}
