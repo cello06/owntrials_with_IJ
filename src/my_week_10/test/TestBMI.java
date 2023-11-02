2bbdd4e1cac37975e0c6cb14b37056ee93aedb2d
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-11-02 21:17:47 +0300

    week_10

---
+++ src/my_week_10/test/TestBMI.java
@@ -0,0 +1,16 @@
+package my_week_10.test;
+
+import my_week_10.classes.BMI;
+
+public class TestBMI {
+    public static void main(String[] args) {
+         BMI bmi1 = new BMI("Kim Yang", 18, 145, 70);
+         System.out.println("The BMI for " + bmi1.getName() + " is "
+                 + bmi1.getBMI() + " " + bmi1.getStatus());
+
+         BMI bmi2 = new BMI("Susan King", 215, 70);
+         System.out.println("The BMI for " + bmi2.getName() + " is "
+                 + bmi2.getBMI() + " " + bmi2.getStatus());
+         }
+         
+}
