dcbef79e57e7bee1fa61e5958228cd298f3a7557
Celalettin harzemşah Aktürk <celalettinakturk06@gmail.com>
2023-11-05 08:01:13 +0300

    week_09

---
+++ src/my_week_10/classes/StackOfIntegers.java
@@ -0,0 +1,41 @@
+package my_week_10.classes;
+
+public class StackOfIntegers {
+    private int[] elements;
+    private int size;
+    private static final int DEFAULT_CAPACITY = 16;
+
+    public StackOfIntegers(int capacity) {
+        elements = new int[capacity];
+    }
+
+    public StackOfIntegers() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public boolean empty() {
+        return size == 0;
+    }
+
+    public int peek() {
+        return elements[size - 1];
+    }
+
+    public void push(int value) {
+        if (size >= elements.length) {
+            int[] temp = new int[elements.length * 2];
+            System.arraycopy(elements, 0, temp, 0, elements.length);
+            elements = temp;
+        }
+        elements[size++] = value;
+    }
+
+    public int pop() {
+        return elements[--size];
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+}
---
+++ src/my_week_10/test/TestStackOfIntegers.java
@@ -0,0 +1,16 @@
+package my_week_10.test;
+
+import my_week_10.classes.StackOfIntegers;
+
+public class TestStackOfIntegers {
+    public static void main(String[] args) {
+        StackOfIntegers stack = new StackOfIntegers();
+        for (int i = 0; i < 10; i++) {
+            stack.push(i);
+        }
+        while (!stack.empty()) {
+            System.out.print(stack.pop() + " ");
+        }
+
+    }
+}
